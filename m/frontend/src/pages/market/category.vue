ce5be21ddc59b3a68d890143cdd022b450157522
苏赵勇 <sutaojie@gmail.com>
2019-09-27 10:49:11 +0800

    商品

--- m/frontend/src/pages/market/category.vue
+++ m/frontend/src/pages/market/category.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    我
+    <van-button type="default">默认按钮</van-button>
+    <div class="goods">
+      <aside class="goods_kinds"></aside>
+      <section class="goods_content">
+        <div class="goods_filters">
+          
+        </div>
+        <div class="goods_search_result">
+          <div class="goods-box">
+            <div class="goods_pic"></div>
+            <span class="goods_name">moumou</span>
+            <span class="goods_price">￥210</span>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 <script>
+import { Button } from 'vant';
 import { getGoodsFilter } from "@/api/category.js";
 export default {
   data() {
@@ -11,11 +27,19 @@ export default {
 
     }
   },
+  components: {
+    [Button.name]: Button,
+  },
   mounted() {
-    console.log(1)
-    getGoodsFilter()
+    // console.log(1)
+    // getGoodsFilter()
   }
 };
 </script>
 <style lang="scss" scope>
+*{
+  margin: 0;
+  border: 0;
+  box-sizing: border-box;
+}
 </style>
---
+++ m/frontend/src/pages/market/goods/detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav class="navigator"></nav>
+    <div class="goods">
+      <!-- 商品轮播图 -->
+      <div class="goods_carousel"></div>
+      <!-- 商品基本信息 -->
+      <div class="goods_base">
+        <div class="goods_tags">
+          <div class="goods_tag">新品</div>
+        </div>
+        <div class="goods_price">
+          <span class="new_price">￥2120</span>
+          <span class="old_price">￥210</span>
+        </div>
+        <span class="goods_name">moumou2019 纯棉时尚收腰型低调 瑜伽服</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  border: 0;
+  box-sizing: border-box;
+}
+</style>
