9996b90620af9f9392a918b02d2639351995c611
苏赵勇 <sutaojie@gmail.com>
2019-10-10 23:28:27 +0800

    添加遮罩层

--- m/frontend/src/App.vue
+++ m/frontend/src/App.vue
@@ -1,18 +1,40 @@
 <template>
   <div id="app">
+    <div class="loding_wrap" v-show="loading" >
+      <van-loading color="#89b264" ref="appLoading"  />
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
+import Vue from "vue";
+import { Loading } from "vant";
+Vue.use(Loading);
 export default {
-  name: 'app'
-}
+  name: "app",
+  computed: {
+    loading() {
+      return this.$store.state.loading
+    }
+  }
+};
 </script>
 
 <style>
 #app {
   background-color: #eee;
+}
+.loding_wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 10000;
+  background: rgba(0, 0, 0, 0.3);
 }
 </style>
--- m/frontend/src/store.js
+++ m/frontend/src/store.js
@@ -5,9 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    loading: true
   },
   mutations: {
-
+    loadStatus (state, flag) {
+      state.loading = flag
+    }
   },
   actions: {
 
