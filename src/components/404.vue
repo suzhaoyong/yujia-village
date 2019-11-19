a6f9a4916a0ee825b86952950ec784c6d3eb3743
苏赵勇 <sutaojie@gmail.com>
2019-11-19 08:25:42 +0800

    返回主页

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="not_fond-wrap">
+    <div class="back_home" @click="() => {this.$router.push('/main');}">返回主页</div>
     <not-found type="not-fond" msg="我寻寻觅觅却找不到该网页"></not-found>  
   </div>
 </template>
@@ -12,6 +13,22 @@ export default {
 <style lang="scss" scoped>
   .not_fond-wrap {
     margin: 0px;
+    position: relative;
+    .back_home{
+      position: absolute;
+      top: 2rem;
+      left: 2rem;
+      width: 7rem;
+      height: 3rem;
+      line-height: 3rem;
+      color: #fff;
+      text-align: center;
+      background: #68B75B;
+      display: inline-block;
+      cursor: pointer;
+      border-radius: 0.2rem;
+      font-size: 0.9rem;
+    }
     /deep/ .type_4 {
       height: 100vh;
     }
