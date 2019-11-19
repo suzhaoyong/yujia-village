6409ca2b4770692f5d3b9ba898d591643dbfb734
苏赵勇 <sutaojie@gmail.com>
2019-11-19 08:17:49 +0800

    404缺省页

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    404  
+  <div class="not_fond-wrap">
+    <not-found type="not-fond" msg="我寻寻觅觅却找不到该网页"></not-found>  
   </div>
 </template>
+<script>
+
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+  .not_fond-wrap {
+    margin: 0px;
+    /deep/ .type_4 {
+      height: 100vh;
+    }
+  }
+  
+</style>
